f365e5fed93187fe297a627b691a659e900f7c66
reshad21 <miakala7@gmail.com>
2024-02-03 12:56:25 +0600

    heor banner section work done

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -6,16 +6,15 @@ const Hero = () => {
     const heroStyle = {
         backgroundImage: `url(${banner})`,
         backgroundSize: 'cover',
-        height: '83vh',
         backgroundPosition: '50% 17%',
     };
 
     return (
         <>
-            <div className="h-[90vh] relative" style={heroStyle}>
-                <div className="bg-slate-900/75 absolute bottom-[4%] left-[20%] w-[20%] text-slate-50 pl-3 py-4 pr-0">
-                    <h1 className='mb-3 font-extrabold text-5xl'>Synth Species</h1>
-                    <Link to="/" className='text-2xl flex items-center gap-1'><span>Learn More</span> <span><MdOutlineKeyboardDoubleArrowRight /></span></Link>
+            <div className="h-[43vh] md:h-[83vh] relative" style={heroStyle}>
+                <div className="bg-slate-900/85 absolute bottom-[4%] left-[4%] md:left-[20%] w-[55%] md:w-[20%] text-slate-50 pl-3 py-4 pr-0">
+                    <h1 className='mb-2 md:mb-3 font-extrabold text-3xl md:text-5xl'>Synth Species</h1>
+                    <Link to="/" className='text-xl md:text-2xl flex items-center gap-1'><span>Learn More</span> <span><MdOutlineKeyboardDoubleArrowRight /></span></Link>
                 </div>
             </div>
         </>
