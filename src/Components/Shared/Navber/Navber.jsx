1e6cbc31ca284ce5bccebcea0f59961eba8865c8
reshad21 <miakala7@gmail.com>
2024-02-05 09:00:01 +0600

    navber work done

--- src/Components/Shared/Navber/Navber.jsx
+++ src/Components/Shared/Navber/Navber.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-
+import navimage from '../../../assets/navber.png';
+import './Navber.css';
 const Navber = () => {
     return (
-        <div>
-            <ul className='flex gap-2'>
-                <li>home</li>
-                <li>home</li>
-                <li>home</li>
-                <li>home</li>
-                <li>home</li>
-            </ul>
-        </div>
+        <>
+            <div className='hidden md:block bg-[#102027]'>
+                <ul className='flex justify-between items-center mx-auto md:max-w-7xl'>
+                    <li className='hidden md:block'><img src={navimage} alt="" /></li>
+                    <li className='flex-1'><a href='#' className='text-center py-3 block text-xl font-normal text-white hover:bg-[#29434e]'>Introduction</a></li>
+                    <li className='flex-1'><a href="#" className='text-center py-3 block text-xl font-normal text-white hover:bg-[#29434e]'>Why Go Synth?</a></li>
+                    <li className='flex-1'><a href="#" className='text-center py-3 block text-xl font-normal text-white hover:bg-[#29434e]'>Features</a></li>
+                    <li className='flex-1'><a href="#" className='text-center py-3 block text-xl font-normal text-white hover:bg-[#29434e]'>Resources</a></li>
+                    <li className='flex-1'><a href="#" className='text-center py-3 block text-xl font-normal text-white bg-[#005cb2]'>Join the Revolution</a></li>
+                    <li className='hidden md:block'><img src={navimage} alt="" /></li>
+                </ul>
+            </div>
+
+            <div className="navbar md:hidden block">
+                <a href="#">Introduction</a>
+                <a href="#">Why Go Synth?</a>
+                <a href="#">Features</a>
+                <a href="#">Resources</a>
+                <a href="#" className="bg-[#005cb2]">Join the Revolution</a>
+            </div>
+
+        </>
     );
 };
 
