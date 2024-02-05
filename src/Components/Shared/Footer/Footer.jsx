b3bdb1fb3e16d80ad254411fad9c94b2778bfb25
reshad21 <miakala7@gmail.com>
2024-02-05 23:25:26 +0600

    footer section add

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <p>This is footer sections</p>
+        <div className='bg-[#29434e] py-7'>
+            <div className="max-w-5xl mx-auto">
+                <p className='text-xl text-white mb-9'>The Synth species was created by <span><a href="#" className='text-[#85c9ff]'>Vader-San</a></span>. The Synth species is open and free to use for any purpose. All artwork on this website is copyright © <span><a href="#" className='text-[#85c9ff]'>Vader-San</a></span>.</p>
+                <p className='text-xl text-white'>This website was created by <span><a href="#" className='text-[#85c9ff]'>Arimah</a></span>, who just thinks Synths are neat.</p>
+            </div>
         </div>
     );
 };
