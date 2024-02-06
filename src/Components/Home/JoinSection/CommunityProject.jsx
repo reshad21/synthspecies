671ec8d6386109e60968538eebb93bd510a8fa59
reshad21 <miakala7@gmail.com>
2024-02-06 14:10:44 +0600

    communityproject component add

---
+++ src/Components/Home/JoinSection/CommunityProject.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CommunityProject = () => {
+    return (
+        <div>
+            <h2 className='text-3xl font-bold py-4'>Community Projects</h2>
+            <p className='text-xl'>In addition to the official links above, you may also check out these fan-operated communities and resources:</p>
+            <ul className='py-8 flex flex-col gap-3 pl-48'>
+                <li><span className='text-xl font-bold'>Fur Affinity:</span><a href='#' className='text-[#005cb2] text-xl'>Synth Revo Gallery by EnDecc</a></li>
+                <li><span className='text-xl font-bold'>VRChat:</span><a href='#' className='text-[#005cb2] text-xl'>Synth Avatar by Intet • Synth Avatar by Lux Aeterna</a></li>
+                <li><span className='text-xl font-bold'>Second Life:</span> <a href='#' className='text-[#005cb2] text-xl'>Synth Avatar Revolution by B.Tech</a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default CommunityProject;
--- src/Components/Home/JoinSection/JoinRevolution.jsx
+++ src/Components/Home/JoinSection/JoinRevolution.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import CommunityProject from './CommunityProject';
 import JoinButton from './JoinButton';
 
 const JoinRevolution = () => {
     return (
         <div className='mx-auto max-w-5xl'>
             <JoinButton />
+            <CommunityProject />
         </div>
     );
 };
