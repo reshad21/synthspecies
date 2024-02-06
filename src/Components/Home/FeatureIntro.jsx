9a06aa22af2c722418a94b7f914ef504a3e2be05
reshad21 <miakala7@gmail.com>
2024-02-06 11:14:26 +0600

    featureComparision responsive issue fixed

--- src/Components/Home/FeatureIntro.jsx
+++ src/Components/Home/FeatureIntro.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const FeatureIntro = () => {
     return (
         <div>
-            <h2 className='text-4xl font-bold py-7 text-[#9e4c00]'>Feature Comparison</h2>
-            <p className='text-xl'>Being a Synth offers many compelling advantages over a traditional organic lifestyle. Here are some highlights:</p>
+            <h2 className='text-4xl font-bold py-7 text-[#9e4c00] px-3 md:px-0'>Feature Comparison</h2>
+            <p className='text-xl px-3 md:px-3'>Being a Synth offers many compelling advantages over a traditional organic lifestyle. Here are some highlights:</p>
         </div>
     );
 };
--- src/Components/Home/FeatureMessage.jsx
+++ src/Components/Home/FeatureMessage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FeatureMessage = () => {
     return (
         <div className='pb-8'>
-            <p className='text-xl'>Still not convinced? Synths have <strong>many more features</strong> than these: read more in our <a href="#" className='text-[#005cb2]'>extensive documentation.</a></p>
+            <p className='text-xl px-3 md:px-0'>Still not convinced? Synths have <strong>many more features</strong> than these: read more in our <a href="#" className='text-[#005cb2]'>extensive documentation.</a></p>
         </div>
     );
 };
